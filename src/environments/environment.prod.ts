9766f13aab1b1ae4b3c3ad7fcbfc69e0fe49c9cf
Vanesa <73238929+vamorilla@users.noreply.github.com>
2022-06-15 17:41:35 -0300

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiOpenWeather: {
-    key: '7becc2ff12ae21582a99f75039107e9f',
+    key: 'your api key',
     url:'https://api.openweathermap.org/data/2.5/'
   }
 };
